d87401fe3074dffef208fc85cf12951c7e580de7
pr4shxnt <student@prashantadhikari7.com.np>
2026-01-27 21:57:08 +0545

    Backend auth related problem solved

--- server/controllers/me.controller.ts
+++ server/controllers/me.controller.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from 'express';
 import Admin from '../models/admin.model.js';
+import Member from "../models/member.model.js"
 import { AuthRequest } from '../middlewares/auth.middleware.js';
 
 export const getMe = async (req: AuthRequest, res: Response) => {
     try {
         const admin = await Admin.findById(req.user?.id).select('-password');
-        if (!admin) {
-            res.status(404).json({ message: 'Admin not found' });
+        const member = await Member.findById(req.user?.id).select('-password');
+        if (!admin || !member) {
+            res.status(404).json({ message: 'Core member not found not found' });
             return;
         }
-        res.status(200).json(admin);
+        res.status(200).json(admin ? admin : member);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching admin data', error });
     }
