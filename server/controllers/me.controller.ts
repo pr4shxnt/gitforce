f029b4469bb9195cd8d430118a36811a85cf7565
pr4shxnt <student@prashantadhikari7.com.np>
2026-01-27 22:01:48 +0545

    Backend auth related problem solved

--- server/controllers/me.controller.ts
+++ server/controllers/me.controller.ts
@@ -7,8 +7,8 @@ export const getMe = async (req: AuthRequest, res: Response) => {
     try {
         const admin = await Admin.findById(req.user?.id).select('-password');
         const member = await Member.findById(req.user?.id).select('-password');
-        if (!admin || !member) {
-            res.status(404).json({ message: 'Core member not found not found' });
+        if (!admin && !member) {
+            res.status(404).json({ message: 'Core member not found' });
             return;
         }
         res.status(200).json(admin ? admin : member);
